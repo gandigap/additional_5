bf8ae7679eb6b0019ef5bd1904f758eb6f53270c
Ihar Berasneu <igor.simatic@gmail.com>
2020-10-12 22:46:13 +0300

    feat:add decision for task brackets

--- src/index.js
+++ src/index.js
@@ -1,32 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let array = [];
-  for(let i in str) {
-    let currentElement = str[i];
-    for(let j in bracketsConfig) {
-      if(currentElement == bracketsConfig[j][1]) {
-        if(bracketsConfig[j][0] != bracketsConfig[j][1]){
-          if(array[array.length - 1] == bracketsConfig[j][0]) {
-            array.pop();
-          } else {
-            return false;
-          }          
-        } else {
-          if(array[array.length - 1] == bracketsConfig[j][0]) {
-            array.pop();
-          } else {
-            array.push(currentElement);
-          }
-        }
-      } else if(currentElement == bracketsConfig[j][0]) {
-        array.push(currentElement);
+  if (str.length % 2 !== 0) return false;
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let count = 0; count < bracketsConfig.length; count++) {
+      if (arr[i] === bracketsConfig[count][0] && arr[i + 1] === bracketsConfig[count][1]) {
+        arr.splice(i, 2);
+        i = -1;
       }
     }
   }
+  return !arr.length;
 
-  if(array.length == 0) {
-    return true;
-  }
-  return false;
+
+  // let array = [];
+  // for (let i in str) {
+  //   let currentElement = str[i];
+  //   for (let j in bracketsConfig) {
+  //     if (currentElement == bracketsConfig[j][1]) {
+  //       if (bracketsConfig[j][0] != bracketsConfig[j][1]) {
+  //         if (array[array.length - 1] == bracketsConfig[j][0]) {
+  //           array.pop();
+  //         } else {
+  //           return false;
+  //         }
+  //       } else {
+  //         if (array[array.length - 1] == bracketsConfig[j][0]) {
+  //           array.pop();
+  //         } else {
+  //           array.push(currentElement);
+  //         }
+  //       }
+  //     } else if (currentElement == bracketsConfig[j][0]) {
+  //       array.push(currentElement);
+  //     }
+  //   }
+  // }
+
+  // if (array.length == 0) {
+  //   return true;
+  // }
+  // return false;
 
 }
